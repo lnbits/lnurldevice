4adcca1db59a1c0589d266c5bb6e4efe8f5d100c
Pieter Meulenhoff <pieter.meulenhoff@eurofins-cybersecurity.com>
2023-07-13 11:07:20 +0200

    Fixed bug where currency is sats

--- lnurl.py
+++ lnurl.py
@@ -112,11 +112,11 @@ async def lnurl_params(
     amount_in_cent = 0
 
     if device.device == "switch":
-        price_msat = (
+        price_msat = int((
             await fiat_amount_as_satoshis(float(amount), device.currency)
             if device.currency != "sat"
-            else amount_in_cent
-        ) * 1000
+            else float(amount)
+        ) * 1000)
 
         # Check they're not trying to trick the switch!
         check = False
